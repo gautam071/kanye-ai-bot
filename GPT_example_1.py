c1d67136a1733dc6eb78642330896bfb9f063b4d
Gautam <gautamr.slm@gmail.com>
2023-05-01 13:41:52 +0530

    Added the code for the YE-Bot
    Added the code for the YE-Bot

---
+++ GPT_example_1.py
@@ -0,0 +1,21 @@
+import openai
+
+# Set up your API key
+openai.api_key = "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"
+
+# Set up the prompt and parameters
+prompt = "What is the meaning of life?"
+model = "text-davinci-002"
+temperature = 0.7
+max_tokens = 100
+
+# Generate text using OpenAI's GPT-3
+response = openai.Completion.create(
+  engine=model,
+  prompt=prompt,
+  temperature=temperature, 
+  max_tokens=max_tokens
+)
+
+# Print the generated text
+print(response.choices[0].text.strip())
